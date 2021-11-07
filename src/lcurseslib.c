51d6ae77d28b4a90019f448d14a62a77f7e820cc
Kartik K. Agaram <vc@akkartik.com>
2021-11-07 06:28:24 -0800

    fix a regression in commit ee85ad384f17

--- src/lcurseslib.c
+++ src/lcurseslib.c
@@ -49,6 +49,7 @@ void draw_menu (lua_State *L) {
   int table = lua_gettop(L);
   if (!lua_istable(L, -1)) {
     lua_pop(L, 1);
+    attroff(A_BOLD);
     return;
   }
   for (lua_pushnil(L); lua_next(L, table) != 0; lua_pop(L, 1))

akkartik/teliva

commit ee85ad384f17e7270f6bfe42ed146406938131a1

simple interface for adding to app menu
We're not going to enforce that the menu items actually do what they
advertise. It's just a way to draw on the bottom line of screen,
something apps aren't otherwise allowed to do.

src/lcurseslib.c
@@ -36,19 +36,31 @@ void draw_menu_item(const char* key, const char* name) {
   attroff(A_REVERSE);
 }
 
-void draw_menu (void) {
+void draw_menu (lua_State *L) {
   attron(A_BOLD|A_REVERSE);
   for (int x = 0; x < COLS; ++x)
     mvaddch(LINES-1, x, ' ');
   menu_column = 2;
   draw_menu_item("^x", "exit");
   draw_menu_item("^e", "edit");
+
+  /* render any app-specific items */
+  lua_getglobal(L, "menu");
+  int table = lua_gettop(L);
+  if (!lua_istable(L, -1)) {
+    lua_pop(L, 1);
+    return;
+  }
+  for (lua_pushnil(L); lua_next(L, table) != 0; lua_pop(L, 1))
+    draw_menu_item(lua_tostring(L, -2), lua_tostring(L, -1));
+
+  attroff(A_BOLD);
 }
 
 
 static int Prefresh (lua_State *L) {
   refresh();
-  draw_menu();
+  draw_menu(L);
   return 1;
 }
 

hanoi.lua
@@ -1,5 +1,6 @@
 local curses = require "curses"
 
+menu = {a="abc"}
 tower = {{6, 5, 4, 3, 2}, {}, {}}
 
 local function len(array)

hanoi.teliva
@@ -1,5 +1,6 @@
 local curses = require "curses"
 
+menu = {a="abc"}
 tower = {{6, 5, 4, 3, 2}, {}, {}}
 
 local function len(array)

src/lua.c
@@ -368,7 +368,7 @@ static int pmain (lua_State *L) {
 }
 
 
-void draw_menu(void);
+void draw_menu(lua_State *);
 char **Argv = NULL;
 
 
@@ -382,7 +382,7 @@ int main (int argc, char **argv) {
   }
   initscr();
   start_color();
-  draw_menu();
+  draw_menu(L);
   echo();
   s.argc = argc;
   s.argv = argv;
@@ -390,6 +390,7 @@ int main (int argc, char **argv) {
   status = lua_cpcall(L, &pmain, &s);
   report(L, status);
   lua_close(L);
+//?   getch();  /* uncomment this to see some common errors if teliva exits cryptically with a non-zero error code. */
   endwin();
   return (status || s.status) ? EXIT_FAILURE : EXIT_SUCCESS;
 }

commit 51d6ae77d28b4a90019f448d14a62a77f7e820cc

fix a regression in commit ee85ad384f17

src/lcurseslib.c
@@ -49,6 +49,7 @@ void draw_menu (lua_State *L) {
   int table = lua_gettop(L);
   if (!lua_istable(L, -1)) {
     lua_pop(L, 1);
+    attroff(A_BOLD);
     return;
   }
   for (lua_pushnil(L); lua_next(L, table) != 0; lua_pop(L, 1))